8f1511a83bc1687371e0918f8284873bab5a58e6
Ramir Mesquita <ramir.mesquita@enovar.com.br>
2018-02-09 16:56:40 -0300

    Sending email and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # users-service
-A Docekrized Spring Boot Service that uses Spring Cloud Configure, Redis Session Cache, RabbitMQ Messages, and other cool stuff 
+A Docekrized Spring Boot Service that uses Spring Cloud Configure, Redis Session Cache, RabbitMQ Messages, and other cool stuff
+ 
+ REST Documentation at
+ https://documenter.getpostman.com/view/1607949/users-service/RVftjXLj#intro
+ 
+ 
--- src/main/java/br/com/ramir/users/service/EmailService.java
+++ src/main/java/br/com/ramir/users/service/EmailService.java
@@ -2,13 +2,50 @@ package br.com.ramir.users.service;
 
 import br.com.ramir.users.exceptions.MessageNotSentException;
 import br.com.ramir.users.messages.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
 
 @Service
 public class EmailService {
 
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 
     public Boolean sendMessage(Message msg) throws MessageNotSentException {
-        return true;
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(msg.getRecipients());
+            message.setSubject("SYSTEM MESSAGE");
+            message.setText(msg.getMessage());
+            emailSender.send(message);
+            return true;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
     }
 }
